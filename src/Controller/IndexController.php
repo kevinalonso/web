1b3d637dfa3f644cfffbb1988efc041f2c907250
kevin <k.alonso@iia-laval.fr>
2022-01-21 23:14:37 +0100

    init project

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Announcement;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+ 
+	/**
+	* @Route("/")
+	*/
+	public function index(): Response
+    {
+        return $this->render('index.html.twig', []);
+    }
+
+}
