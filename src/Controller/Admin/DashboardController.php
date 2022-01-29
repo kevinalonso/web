48fa416d1c9dd572503f4e755d3e46205d639b94
kevin <k.alonso@iia-laval.fr>
2022-01-29 16:35:18 +0100

    Add controller require admin - création easy_admin.yaml - création du controller DashboardController.php

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+}
